ab6f8e526009903ac25ac4c6cd3c823e87a04027
dineshigdd <dineshigdd@gmail.com>
2022-05-18 15:43:19 -0700

    add new files

---
+++ src/App.js
@@ -0,0 +1,144 @@
+import "./styles.css";
+import Map from "./Map/Map";
+import { useEffect, useState } from "react";
+export default function App() {
+  const [coords, setCorrds] = useState({
+    latitude: 39.7837304,
+    longitude: -100.4458825
+  });
+  const [display_name, setName] = useState("");
+
+  const [address, setAddress] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log("Lon:" + position.coords.longtitude);
+      setCorrds({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });     
+      
+     
+    });
+  }, [ coords]);
+
+  // function getCurrentCityName() {
+  //   let url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2
+  //   &lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  //   fetch(url, {
+  //     method: "GET",
+  //     mode: "cors",
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
+  //     }
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => console.log( data));
+  // }
+  //separete the entred string
+  function update(field) {
+    return (e) => {
+      const value = e.currentTarget.value;
+      setAddress((address) => ({ ...address, [field]: value }));
+    };
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(address);
+
+    let url = `https://nominatim.openstreetmap.org/search?
+    street=${address.street}
+    &city=${address.city}
+    &state=${address.state}
+    &country=${address.country}
+    &postalcode=${address.postalcode}&format=json`;
+
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
+      }
+    })
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          setName(data[0].display_name);
+          setCorrds({
+            latitude: data[0].lat,
+            longitude: data[0].lon
+          });
+        }
+        // console.log(Object.keys(data[0])
+
+        //   setCorrds(
+        //   {
+        //   'latitude':data[0].lat,
+        //   'longitude': data[0].lon
+        // });
+
+        // setInfo({
+        //   dispaly_name: data[0].dispaly_name,
+        //   icon: data[0].icon
+        // })
+      );
+  }
+
+  return (
+    <div className="App">
+      <h1>Enter The address</h1>
+      <section className="form-container">
+        <form>
+          <label>street:</label>
+          <input
+            value={address.street}
+            placeholder="1234 abc street"
+            onChange={update("street")}
+            id="street"
+            type="text"
+          />
+          <label>city:</label>
+          <input
+            placeholder="1234 abc street"
+            type="text"
+            value={address.city}
+            onChange={update("city")}
+            id="city"
+          />
+          <br />
+          <label>State:</label>
+          <input
+            placeholder="CA"
+            type="text"
+            value={address.state}
+            onChange={update("state")}
+            id="state"
+          />
+          <label>zip code:</label>
+          <input
+            placeholder="91423"
+            type="text"
+            value={address.postalcode}
+            onChange={update("postalcode")}
+            id="postalcode"
+          />
+          <br />
+          <label>Country:</label>
+          <input
+            placeholder="USA"
+            type="text"
+            value={address.country}
+            onChange={update("country")}
+            id="country"
+          />
+          <br />
+
+          <button onClick={(e) => submitHandler(e)}>Search</button>
+        </form>
+      </section>
+      <Map coords={coords} info={display_name} />
+    </div>
+  );
+}
---
+++ src/Map/Map.jsx
@@ -0,0 +1,47 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import icon from "../Images/icon.png";
+import L from "leaflet";
+
+export default function Map({ coords, info }) {
+  console.log(info);
+  const { latitude, longitude } = coords;
+  const { display_name } = info;
+  console.log(latitude);
+  console.log(longitude);
+  // console.log(longitude );
+  // const { lat, long } = coords;
+
+  const customIcon = new L.Icon({
+    iconUrl: icon,
+    iconSize: [25, 35],
+    iconAnchor: [5, 30]
+  });
+
+  function MapView() {
+    let map = useMap();
+    map.setView([latitude, longitude], map.getZoom());
+
+    return null;
+  }
+
+  return (
+    <MapContainer
+      classsName="map"
+      center={[latitude, longitude]}
+      zoom={10}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
+        contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker icon={customIcon} position={[latitude, longitude]}>
+        <Popup>{display_name}</Popup>
+      </Marker>
+      <MapView />
+    </MapContainer>
+  );
+}
